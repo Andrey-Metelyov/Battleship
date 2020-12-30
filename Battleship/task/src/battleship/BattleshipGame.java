a803cf52cea244ba62755787714d8bac874125a2
Andrey-Metelyov <andrey.metelyov@gmail.com>
2020-12-31 01:26:10 +0900

    2/5 complete

--- Battleship/task/src/battleship/BattleshipGame.java
+++ Battleship/task/src/battleship/BattleshipGame.java
@@ -1,12 +1,23 @@
 package battleship;
 
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class BattleshipGame {
     private final BattleshipView battleshipView;
     private final String[][] field;
     private final Scanner scanner = new Scanner(System.in);
+
+    class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public BattleshipGame() {
         this.battleshipView = new BattleshipView(this);
@@ -18,6 +29,51 @@ public class BattleshipGame {
 
     public void start() {
         placeShips();
+        startGame();
+    }
+
+    private void startGame() {
+        System.out.println("\nThe game starts!");
+        battleshipView.showField();
+        takeShot();
+    }
+
+    private void takeShot() {
+        while (true) {
+            System.out.println("\nTake a shot!");
+            String coordinate = scanner.nextLine();
+            Optional<Point> point = checkCoordinate(coordinate);
+            if (point.isEmpty()) {
+                System.out.println("Error! You entered the wrong coordinates! Try again:");
+                continue;
+            }
+            if (field[point.get().x][point.get().y].equals("O")) {
+                field[point.get().x][point.get().y] = "X";
+                battleshipView.showField();
+                System.out.println("\nYou hit a ship!");
+            } else {
+                field[point.get().x][point.get().y] = "M";
+                battleshipView.showField();
+                System.out.println("\nYou missed!");
+            }
+        }
+    }
+
+    private Optional<Point> checkCoordinate(String coordinate) {
+        int x = coordinate.charAt(0) - 'A';
+        if (x < 0 || x > 9) {
+            return Optional.empty();
+        }
+        int y;
+        try {
+            y = Integer.parseInt(coordinate.substring(1)) - 1;
+            if (y < 0 || y > 9) {
+                return Optional.empty();
+            }
+        } catch (NumberFormatException exception) {
+            return Optional.empty();
+        }
+        return Optional.of(new Point(x, y));
     }
 
     private void placeShips() {
@@ -42,26 +98,16 @@ public class BattleshipGame {
                 System.out.println("Error");
                 continue;
             }
-            int x1 = coordinates[0].charAt(0) - 'A';
-            int x2 = coordinates[1].charAt(0) - 'A';
-            if (x1 < 0 || x1 > 9 || x2 < 0 || x2 > 9) {
+            Optional<Point> startPoint = checkCoordinate(coordinates[0]);
+            Optional<Point> endPoint = checkCoordinate(coordinates[1]);
+            if (startPoint.isEmpty() || endPoint.isEmpty()) {
                 System.out.println("Error");
                 continue;
             }
-            int y1;
-            int y2;
-            try {
-                y1 = Integer.parseInt(coordinates[0].substring(1)) - 1;
-                y2 = Integer.parseInt(coordinates[1].substring(1)) - 1;
-            } catch (NumberFormatException exception) {
-                System.out.println("Error");
-                continue;
-            }
-            if (y1 < 0 || y1 > 9 || y2 < 0 || y2 > 9) {
-                System.out.println("Error");
-                continue;
-            }
-//            System.out.println(x1 + " " + y1 + " " + x2 + " " + y2);
+            int x1 = startPoint.get().x;
+            int x2 = endPoint.get().x;
+            int y1 = startPoint.get().y;
+            int y2 = endPoint.get().y;
 
             if (x1 != x2 && y1 != y2) {
                 System.out.println("Error");
